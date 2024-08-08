8a86847df1825c98a34a986dfdd3b1906955ac13
Jc Abalos <jc4balos@gmail.com>
2024-08-08 16:15:01 +0800

    added modifyuserinfo interface

--- src/main/java/com/jc4balos/user_service/service/users/v1/UserService.java
+++ src/main/java/com/jc4balos/user_service/service/users/v1/UserService.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import com.jc4balos.user_service.dto.user.ModifyUserInfoDto;
 import com.jc4balos.user_service.dto.user.NewUserDto;
 
 @Service
@@ -11,4 +12,6 @@ public interface UserService {
     Map<String, String> createUser(NewUserDto newUserDto);
 
     Map<String, Object> getAllUsers(int pageIndex, int itemsPerPage, String searchParam, String sortBy, String order);
+
+    Map<String, String> modifyUserInfo(Long userId, ModifyUserInfoDto modifyUserInfoDto);
 }
