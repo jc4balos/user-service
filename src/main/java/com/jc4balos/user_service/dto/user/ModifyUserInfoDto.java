81a46fd5610c96aa685417f6387292972aec74b6
Jc Abalos <jc4balos@gmail.com>
2024-08-08 16:14:50 +0800

    edited modifyb userinfodto

--- src/main/java/com/jc4balos/user_service/dto/user/ModifyUserInfoDto.java
+++ src/main/java/com/jc4balos/user_service/dto/user/ModifyUserInfoDto.java
@@ -2,7 +2,6 @@ package com.jc4balos.user_service.dto.user;
 
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -14,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ModifyUserDto {
+public class ModifyUserInfoDto {
 
     @NotNull(message = "First name is required.")
     private String firstName;
@@ -26,24 +25,11 @@ public class ModifyUserDto {
 
     private String husbandSurname;
 
-    @NotEmpty(message = "Username is required.")
-    private String username;
-
-    @NotEmpty(message = "Password is required.")
-    private String password;
-
-    @NotEmpty(message = "Email is required.")
-    @Email(message = "Email should be valid")
-    private String email;
-
     private String addressLine1;
 
     private String addressLine2;
 
     private String addressLine3;
-
-    @NotEmpty(message = "Contact Number is required.")
-    private String contactNumber;
 
     @NotNull(message = "Birthdate is required.")
     private LocalDate birthDate;
@@ -51,6 +37,4 @@ public class ModifyUserDto {
     @NotEmpty(message = "Sex is required.")
     private String sex;
 
-    @NotNull(message = "Please set the user status")
-    private boolean isActive;
 }
