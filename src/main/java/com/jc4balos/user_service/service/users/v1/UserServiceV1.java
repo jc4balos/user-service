79c360c82234ec66f17684ee684145a4e2ca5aae
Jc Abalos <jc4balos@gmail.com>
2024-08-19 09:44:20 +0800

    deprecated all  v1

--- src/main/java/com/jc4balos/user_service/service/users/v1/UserServiceV1.java
+++ src/main/java/com/jc4balos/user_service/service/users/v1/UserServiceV1.java
@@ -9,9 +9,30 @@ import com.jc4balos.user_service.dto.user.NewUserDto;
 
 @Service
 public interface UserServiceV1 {
+    //
+    /**
+     * @deprecated
+     *             This method is deprecated due to it is not asynchronous (doesn't
+     *             use threading)
+     */
+    @Deprecated
     Map<String, String> createUser(NewUserDto newUserDto);
 
+    //
+    /**
+     * @deprecated
+     *             This method is deprecated due to it is not asynchronous (doesn't
+     *             use threading)
+     */
+    @Deprecated
     Map<String, Object> getAllUsers(int pageIndex, int itemsPerPage, String searchParam, String sortBy, String order);
 
+    //
+    /**
+     * @deprecated
+     *             This method is deprecated due to it is not asynchronous (doesn't
+     *             use threading)
+     */
+    @Deprecated
     Map<String, String> modifyUserInfo(Long userId, ModifyUserInfoDto modifyUserInfoDto);
 }
